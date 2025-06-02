1cde2fbb6352d55d64317e7a352def022ba3746a
earthastro <214470886+earthastro@users.noreply.github.com>
2025-06-02 20:42:41 +0100

    Update colours and styles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeColour() {
-  const colours = ['#ffadad', '#caffbf', '#9bf6ff', '#ffd6a5', '#bdb2ff'];
+  const colours = ['#ffadad', '#caffbf', '#9bf6ff', '#ffd6a5', '#bdb2ff', '#fefae0', '#a0c4ff', '#fdffb6'];
   const random = colours[Math.floor(Math.random() * colours.length)];
   document.body.style.backgroundColor = random;
 }
